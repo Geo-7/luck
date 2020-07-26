bd0b1c4fac9a11e00b133fc6ae5f153f1b539879
Geo-7 <george@1w1g.com>
2020-07-26 16:30:48 +0430

    Make postgresql integration test multithread

--- spec/luck_spec.cr
+++ spec/luck_spec.cr
@@ -29,38 +29,38 @@ describe APIParser do
     it "Gets a json and make query to create corrosponding table" do
       ap.db_engine = "sqlite3"
       input_json = JSON.parse(%({"legs": "TEXT", "att": "TEXT", "hands": "INTEGER"}))
-      str,err = ap.make_create_table_str("Monkey", input_json)
+      str, err = ap.make_create_table_str("Monkey", input_json)
       str.should eq "CREATE TABLE Monkey(id INTEGER PRIMARY KEY, legs TEXT, att TEXT, hands INTEGER)"
       err.should be_false
       input_json = JSON.parse(%({"name": "TEXT", "genre": "TEXT"}))
-      str,err = ap.make_create_table_str("Movie", input_json)
+      str, err = ap.make_create_table_str("Movie", input_json)
       str.should eq "CREATE TABLE Movie(id INTEGER PRIMARY KEY, name TEXT, genre TEXT)"
       err.should be_false
       input_json = JSON.parse(%({"legs": "TEXT", "att": "TEXT", "hands": "INTEGER", "ID": "serial"}))
-      str,err = ap.make_create_table_str("Monkey", input_json)
+      str, err = ap.make_create_table_str("Monkey", input_json)
       str.should eq "CREATE TABLE Monkey(id INTEGER PRIMARY KEY, legs TEXT, att TEXT, hands INTEGER, ID serial)"
       err.should be_true
       input_json = JSON.parse(%({"legs": "TEXT", "att": "TEXT", "hands": "INTEGER", "id": "TEXT"}))
-      str,err = ap.make_create_table_str("Monkey", input_json)
+      str, err = ap.make_create_table_str("Monkey", input_json)
       str.should eq "CREATE TABLE Monkey(id INTEGER PRIMARY KEY, legs TEXT, att TEXT, hands INTEGER, id TEXT)"
       err.should be_true
       ap.db_engine = "postgres"
       input_json = JSON.parse(%({"name": "varchar", "genre": "varchar"}))
-      str,err = ap.make_create_table_str("Movie", input_json)
+      str, err = ap.make_create_table_str("Movie", input_json)
       str.should eq "CREATE TABLE Movie(id SERIAL, name varchar, genre varchar)"
       err.should be_false
-      str,err = ap.make_create_table_str("Mov%$ie", input_json)
+      str, err = ap.make_create_table_str("Mov%$ie", input_json)
       str.should eq "CREATE TABLE Movie(id SERIAL, name varchar, genre varchar)"
       err.should be_false
       input_json = JSON.parse(%({"na@me": "varchar", "g!enre": "varchar"}))
-      str,err = ap.make_create_table_str("Mov%$ie", input_json)
+      str, err = ap.make_create_table_str("Mov%$ie", input_json)
       str.should eq "CREATE TABLE Movie(id SERIAL, name varchar, genre varchar)"
       err.should be_false
-      str,err = ap.make_create_table_str("movieäß", input_json)
+      str, err = ap.make_create_table_str("movieäß", input_json)
       str.should eq "CREATE TABLE movieäß(id SERIAL, name varchar, genre varchar)"
       err.should be_false
       input_json = JSON.parse(%({"name": "varchar", "genre": "varchar", "id": "varchar"}))
-      str,err = ap.make_create_table_str("Movie", input_json)
+      str, err = ap.make_create_table_str("Movie", input_json)
       str.should eq "CREATE TABLE Movie(id SERIAL, name varchar, genre varchar, id varchar)"
       err.should be_true
     end
@@ -81,11 +81,11 @@ describe APIParser do
     it "Get a json and create update query" do
       input_json = JSON.parse(%({"id": 1,"name": "Matrix", "genre": "SCI-FI"}))
       ap.db_engine = "sqlite3"
-      str,args = ap.make_update_str("Movie", input_json)
+      str, args = ap.make_update_str("Movie", input_json)
       str.should eq "UPDATE Movie SET name=?, genre=? WHERE id=?"
       args.should eq ["Matrix", "SCI-FI", "1"]
       ap.db_engine = "postgres"
-      str,args = ap.make_update_str("Movie", input_json)
+      str, args = ap.make_update_str("Movie", input_json)
       str.should eq "UPDATE Movie SET name=$1, genre=$2 WHERE id=$3"
       args.should eq ["Matrix", "SCI-FI", "1"]
     end
@@ -104,17 +104,17 @@ end
 integration_test = ENV["integration_test"] ||= "false"
 table_name = "movie" + Time.utc.to_s("%s")
 if integration_test == "sqlite3"
-  channel =Channel(Nil).new
+  channel = Channel(Nil).new
   spawn same_thread: false do
     ap = APIParser.new(*LuckConfig.get_env)
-    ap.start()
+    ap.start
   end
   spawn same_thread: false do
-    i =20
-    while i >0
+    i = 10
+    while i > 0
       pp "#{i} seconds to start integration test of luck CMS"
       sleep(1)
-      i-=1 
+      i -= 1
     end
     describe "POST /object/table_name" do
       it "POST a table json and make a table" do
@@ -123,8 +123,8 @@ if integration_test == "sqlite3"
         response.body.should eq "DB::ExecResult(@rows_affected=0, @last_insert_id=0)"
       end
       it "POST an invalid json for creating table" do
-        data =%({"name": "TEXT", "id": "TEXT"})
-        response = HTTP::Client.post("http://127.0.0.1:5800/object/#{table_name}",HTTP::Headers{"User-Agent" => "Crystal"},data)
+        data = %({"name": "TEXT", "id": "TEXT"})
+        response = HTTP::Client.post("http://127.0.0.1:5800/object/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, data)
         response.body.should eq %({"error":true,"description":"could not create table"})
       end
     end
@@ -152,7 +152,7 @@ if integration_test == "sqlite3"
     describe "UPDATE /table_name" do
       it "It will update a record in database" do
         update_json = (%({"id": 2,"name": "Matrix", "genre": "SCI-FI"}))
-        response =HTTP::Client.patch("127.0.0.1:5800/#{table_name}",HTTP::Headers{"User-Agent" => "Crystal"}, update_json)
+        response = HTTP::Client.patch("127.0.0.1:5800/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, update_json)
         response.body.should eq "DB::ExecResult(@rows_affected=1, @last_insert_id=2)"
       end
     end
@@ -162,44 +162,59 @@ if integration_test == "sqlite3"
 end
 integration_test = ENV["integration_test"] ||= "false"
 if integration_test == "postgres"
-  describe "POST /object/table_name" do
-    it "POST a table json and make a table" do
-      data = %({"name": "varchar", "genre": "varchar"})
-      response = HTTP::Client.post("127.0.0.1:5800/object/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, data)
-      response.body.should eq "DB::ExecResult(@rows_affected=0, @last_insert_id=0)"
+  channel = Channel(Nil).new
+  spawn do
+    ap = APIParser.new(*LuckConfig.get_env)
+    ap.start
+  end
+  spawn do
+    i = 10
+    while i > 0
+      puts "#{i} seoconds to start integration test of luck CMS"
+      sleep(1)
+      i -= 1
     end
-  end
-  describe "POST /table_name" do
-    it "Insert data to a table with a POST" do
-      data = %({"name": "her", "genre": "SCI-FI"})
-      response = HTTP::Client.post("127.0.0.1:5800/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, data)
-      response.body.should eq "DB::ExecResult(@rows_affected=1, @last_insert_id=0)"
+    describe "POST /object/table_name" do
+      it "POST a table json and make a table" do
+        data = %({"name": "varchar", "genre": "varchar"})
+        response = HTTP::Client.post("127.0.0.1:5800/object/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, data)
+        response.body.should eq "DB::ExecResult(@rows_affected=0, @last_insert_id=0)"
+      end
     end
-  end
-  describe "POST /table_name" do
-    it "Insert data to a table with a POST" do
-      data = %({"name": "her", "genre": "Romance"})
-      response = HTTP::Client.post("127.0.0.1:5800/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, data)
-      response.body.should eq "DB::ExecResult(@rows_affected=1, @last_insert_id=0)"
+    describe "POST /table_name" do
+      it "Insert data to a table with a POST" do
+        data = %({"name": "her", "genre": "SCI-FI"})
+        response = HTTP::Client.post("127.0.0.1:5800/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, data)
+        response.body.should eq "DB::ExecResult(@rows_affected=1, @last_insert_id=0)"
+      end
     end
-  end
-  describe "DELETE /table_name" do
-    it "It will delete a record from database with an its ID" do
-      delete_json = %({"id": 1})
-      response = HTTP::Client.delete("127.0.0.1:5800/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, delete_json)
-      response.body.should eq "DB::ExecResult(@rows_affected=1, @last_insert_id=0)"
+    describe "POST /table_name" do
+      it "Insert data to a table with a POST" do
+        data = %({"name": "her", "genre": "Romance"})
+        response = HTTP::Client.post("127.0.0.1:5800/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, data)
+        response.body.should eq "DB::ExecResult(@rows_affected=1, @last_insert_id=0)"
+      end
     end
-  end
-  describe "UPDATE /table_name" do
-    it "It will update a record in database" do
-      update_json = (%({"id": 2,"name": "Matrix", "genre": "SCI-FI"}))
-      response =HTTP::Client.patch("127.0.0.1:5800/#{table_name}",HTTP::Headers{"User-Agent" => "Crystal"}, update_json)
-      response.body.should eq "DB::ExecResult(@rows_affected=1, @last_insert_id=0)"
+    describe "DELETE /table_name" do
+      it "It will delete a record from database with an its ID" do
+        delete_json = %({"id": 1})
+        response = HTTP::Client.delete("127.0.0.1:5800/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, delete_json)
+        response.body.should eq "DB::ExecResult(@rows_affected=1, @last_insert_id=0)"
+      end
     end
+    describe "UPDATE /table_name" do
+      it "It will update a record in database" do
+        update_json = (%({"id": 2,"name": "Matrix", "genre": "SCI-FI"}))
+        response = HTTP::Client.patch("127.0.0.1:5800/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, update_json)
+        response.body.should eq "DB::ExecResult(@rows_affected=1, @last_insert_id=0)"
+      end
+    end
+    it "POST an invalid json for creating table" do
+      data = %({"name": "varchar", "id": "varchar"})
+      response = HTTP::Client.post("http://127.0.0.1:5800/object/#{table_name}", HTTP::Headers{"User-Agent" => "Crystal"}, data)
+      response.body.should eq %({"error":true,"description":"could not create table"})
+    end
+    channel.send(nil)
   end
-  it "POST an invalid json for creating table" do
-    data =%({"name": "varchar", "id": "varchar"})
-    response = HTTP::Client.post("http://127.0.0.1:5800/object/#{table_name}",HTTP::Headers{"User-Agent" => "Crystal"},data)
-    response.body.should eq %({"error":true,"description":"could not create table"})
-  end
+  channel.receive
 end
